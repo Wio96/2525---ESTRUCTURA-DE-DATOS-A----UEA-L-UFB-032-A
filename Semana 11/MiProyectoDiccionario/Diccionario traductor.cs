49cedd263c19d263cb2ba6633da01911b84b2a52
Usuario <wiogualinga96@gmail.com>
2025-10-04 11:20:53 -0500

    ultimos arreglos de codigo

--- Semana 11/MiProyectoDiccionario/Diccionario traductor.cs
+++ Semana 11/MiProyectoDiccionario/Diccionario traductor.cs
@@ -1,85 +1,76 @@
-﻿class Program
+﻿// Diccionario inicial Español -> Inglés
+var diccionario = new Dictionary<string, string>()
 {
-    static void Main()
+    {"tiempo","time"},
+    {"persona","person"},
+    {"año","year"},
+    {"camino","way"},
+    {"día","day"},
+    {"cosa","thing"},
+    {"hombre","man"},
+    {"mundo","world"},
+    {"vida","life"},
+    {"mano","hand"},
+    {"parte","part"},
+    {"niño","child"},
+    {"ojo","eye"},
+    {"mujer","woman"},
+    {"lugar","place"},
+    {"trabajo","work"},
+    {"semana","week"},
+    {"caso","case"},
+    {"punto","point"},
+    {"gobierno","government"}
+};
+
+int opcion;
+do
+{
+    Console.WriteLine("\n===== MENÚ =====");
+    Console.WriteLine("1. Traducir una frase");
+    Console.WriteLine("2. Agregar palabras al diccionario");
+    Console.WriteLine("0. Salir");
+    Console.Write("Seleccione una opción: ");
+    int.TryParse(Console.ReadLine(), out opcion);
+
+    if (opcion == 1)
     {
-        // Diccionario inicial Español -> Inglés
-        var diccionario = new System.Collections.Generic.Dictionary<string, string>()
+        Console.Write("Frase: ");
+        string frase = Console.ReadLine() ?? "";
+        string[] palabras = frase.Split(' ');
+
+        for (int i = 0; i < palabras.Length; i++)
         {
-            {"tiempo","time"},
-            {"persona","person"},
-            {"año","year"},
-            {"camino","way"},
-            {"día","day"},
-            {"cosa","thing"},
-            {"hombre","man"},
-            {"mundo","world"},
-            {"vida","life"},
-            {"mano","hand"},
-            {"parte","part"},
-            {"niño","child"},
-            {"ojo","eye"},
-            {"mujer","woman"},
-            {"lugar","place"},
-            {"trabajo","work"},
-            {"semana","week"},
-            {"caso","case"},
-            {"punto","point"},
-            {"gobierno","government"}
-        };
+            string token = palabras[i];
+            string palabra = token.ToLower(); // Sin Trim si tu profe no lo pidió
 
-        int opcion;
-        do
-        {
-            Console.WriteLine("\n===== MENÚ =====");
-            Console.WriteLine("1. Traducir una frase");
-            Console.WriteLine("2. Agregar palabras al diccionario");
-            Console.WriteLine("0. Salir");
-            Console.Write("Seleccione una opción: ");
-            int.TryParse(Console.ReadLine(), out opcion);
-
-            if (opcion == 1)
+            if (diccionario.ContainsKey(palabra))
             {
-                Console.Write("Frase: ");
-                string frase = Console.ReadLine() ?? "";
-                string[] palabras = frase.Split(' ');
-
-                for (int i = 0; i < palabras.Length; i++)
-                {
-                    string token = palabras[i];
-                    string palabra = token.TrimEnd(',', '.', ';', ':', '!', '?').ToLower();
-
-                    if (diccionario.ContainsKey(palabra))
-                    {
-                        string tradu = diccionario[palabra];
-                        // Conserva mayúscula inicial
-                        if (char.IsUpper(token[0]))
-                            tradu = char.ToUpper(tradu[0]) + tradu.Substring(1);
-                        palabras[i] = tradu + token.Substring(palabra.Length);
-                    }
-                }
-
-                Console.WriteLine("Traducción (parcial): " + string.Join(" ", palabras));
+                string tradu = diccionario[palabra];
+                if (char.IsUpper(token[0]))
+                    tradu = char.ToUpper(tradu[0]) + tradu.Substring(1);
+                palabras[i] = tradu;
             }
-            else if (opcion == 2)
-            {
-                Console.Write("Palabra en español: ");
-                string esp = Console.ReadLine()?.ToLower() ?? "";
-                Console.Write("Traducción en inglés: ");
-                string ing = Console.ReadLine()?.ToLower() ?? "";
+        }
 
-                if (!diccionario.ContainsKey(esp))
-                {
-                    diccionario.Add(esp, ing);
-                    Console.WriteLine("Palabra agregada correctamente.");
-                }
-                else
-                {
-                    Console.WriteLine("La palabra ya existe en el diccionario.");
-                }
-            }
-
-        } while (opcion != 0);
-
-        Console.WriteLine("¡Hasta luego!");
+        Console.WriteLine("Traducción (parcial): " + string.Join(" ", palabras));
     }
-}
+    else if (opcion == 2)
+    {
+        Console.Write("Palabra en español: ");
+        string esp = Console.ReadLine() ?? "";
+        Console.Write("Traducción en inglés: ");
+        string ing = Console.ReadLine() ?? "";
+
+        if (!diccionario.ContainsKey(esp))
+        {
+            diccionario.Add(esp, ing);
+            Console.WriteLine("Palabra agregada correctamente.");
+        }
+        else
+            Console.WriteLine("La palabra ya existe en el diccionario.");
+    }
+
+} while (opcion != 0);
+
+Console.WriteLine("¡Hasta luego!");
